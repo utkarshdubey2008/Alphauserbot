e69b4ee089c485747f54b740038e880c35cb1bd2
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-28 15:25:16 +0530

    Update joke.py

--- plugin/joke.py
+++ plugin/joke.py
@@ -1,8 +1,8 @@
-import requests
 from telethon import events
+import requests
 
 @events.register(events.NewMessage(pattern=r'\.joke'))
 async def joke(event):
-    response = requests.get("https://official-joke-api.appspot.com/jokes/random")
+    response = requests.get("https://official-joke-api.appspot.com/random_joke")
     joke = response.json()
-    await event.reply(f"{joke['setup']} {joke['punchline']}")
+    await event.reply(f"{joke['setup']}\n{joke['punchline']}")
