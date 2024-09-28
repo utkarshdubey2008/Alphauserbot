ff0f5daf3bdf486bbe95997d95f3f46d9c76577f
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-28 17:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from telethon import TelegramClient, events
 import os
+import importlib
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file if needed
 load_dotenv()
 
-API_ID = os.getenv('29264175')
-API_HASH = os.getenv('0a3e78a4cd29529769faf5fe588f6de7')
-SESSION_STRING = os.getenv('1BVtsOMMBu3xe1ZW0g9EiC24eZzCPmnX14nRc4s51_U8J5BO0DKYQNzaFgVGiFnApvFHQ4FxFcjfQ-_ZWFTtGTte7f77uPS12qYd7cqUtPs0DkgO9bzHN1LQy64GxaX1Jw4k_5rwwOiNm_PmK2MDV09-e1OMKTeyzvptOA6_O1xtHUE_w8ANai1NjrlJTVeQ04paoK8QiV0lGmDIubYucl7extnM1hozkNd5D0fqiapEoSeON7Y0I7LkalcY8-0EOQGa87hD8tSzd3xD2d7nPS4CJEDqOav7dIFZ8tZ4Pg13ZFaNPpMJ-owRECP8mmGcr-kPE6Q7y300BlSpFlsQYfiaw5xXa07o=')
+# Use os.environ.get to retrieve environment variables
+API_ID = os.environ.get('API_ID', '29264175')  # Default value for local testing
+API_HASH = os.environ.get('API_HASH', '0a3e78a4cd29529769faf5fe588f6de7')  # Default value for local testing
+SESSION_STRING = os.environ.get('SESSION_STRING', 'your_default_session_string')  # Replace with a valid session string if needed
 
 client = TelegramClient('user_bot', API_ID, API_HASH).start(session=SESSION_STRING)
 
