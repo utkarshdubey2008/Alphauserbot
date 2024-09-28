384c9fab0456b28eefa9fa3530d89ecf2d840ae8
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-28 17:32:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from telethon import TelegramClient, events
 import os
-from dotenv import load_dotenv
 import importlib
-
-load_dotenv()
-
-API_ID = os.environ.get('API_ID', '29264175')  # Your actual API ID
-API_HASH = os.environ.get('API_HASH', '0a3e78a4cd29529769faf5fe588f6de7')  # Your actual API hash
-SESSION_STRING = os.environ.get('SESSION_STRING', '1BVtsOMMBu3xe1ZW0g9EiC24eZzCPmnX14nRc4s51_U8J5BO0DKYQNzaFgVGiFnApvFHQ4FxFcjfQ-_ZWFTtGTte7f77uPS12qYd7cqUtPs0DkgO9bzHN1LQy64GxaX1Jw4k_5rwwOiNm_PmK2MDV09-e1OMKTeyzvptOA6_O1xtHUE_w8ANai1NjrlJTVeQ04paoK8QiV0lGmDIubYucl7extnM1hozkNd5D0fqiapEoSeON7Y0I7LkalcY8-0EOQGa87hD8tSzd3xD2d7nPS4CJEDqOav7dIFZ8tZ4Pg13ZFaNPpMJ-owRECP8mmGcr-kPE6Q7y300BlSpFlsQYfiaw5xXa07o=')  # Your actual session string
+from config import API_ID, API_HASH, SESSION_STRING  # Import from config
 
 client = TelegramClient('user_bot', API_ID, API_HASH).start()
 
