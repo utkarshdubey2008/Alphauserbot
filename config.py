93eaeac7bed948335a785b91a8c942ca1297039e
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-28 16:32:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Configuration file for the Telegram user bot
 
 # Your API ID (obtainable from my.telegram.org)
-API_ID = 'YOUR_API_ID'  # Replace with your actual API ID
+API_ID = '29264175'  # Replace with your actual API ID
 
 # Your API hash (obtainable from my.telegram.org)
-API_HASH = 'YOUR_API_HASH'  # Replace with your actual API hash
+API_HASH = '0a3e78a4cd29529769faf5fe588f6de7'  # Replace with your actual API hash
 
 # Your session string (you can obtain this by logging in through Telethon)
-SESSION_STRING = 'YOUR_SESSION_STRING'  # Replace with your session string
+SESSION_STRING = '1BVtsOMMBu3xe1ZW0g9EiC24eZzCPmnX14nRc4s51_U8J5BO0DKYQNzaFgVGiFnApvFHQ4FxFcjfQ-_ZWFTtGTte7f77uPS12qYd7cqUtPs0DkgO9bzHN1LQy64GxaX1Jw4k_5rwwOiNm_PmK2MDV09-e1OMKTeyzvptOA6_O1xtHUE_w8ANai1NjrlJTVeQ04paoK8QiV0lGmDIubYucl7extnM1hozkNd5D0fqiapEoSeON7Y0I7LkalcY8-0EOQGa87hD8tSzd3xD2d7nPS4CJEDqOav7dIFZ8tZ4Pg13ZFaNPpMJ-owRECP8mmGcr-kPE6Q7y300BlSpFlsQYfiaw5xXa07o='  # Replace with your session string
