12a45fa69abcc3e9f0fdd7fe64ae44d4e3e0c512
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-28 16:52:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv('API_ID')
-API_HASH = os.getenv('API_HASH')
-SESSION_STRING = os.getenv('SESSION_STRING')
+API_ID = os.getenv('29264175')
+API_HASH = os.getenv('0a3e78a4cd29529769faf5fe588f6de7')
+SESSION_STRING = os.getenv('1BVtsOMMBu3xe1ZW0g9EiC24eZzCPmnX14nRc4s51_U8J5BO0DKYQNzaFgVGiFnApvFHQ4FxFcjfQ-_ZWFTtGTte7f77uPS12qYd7cqUtPs0DkgO9bzHN1LQy64GxaX1Jw4k_5rwwOiNm_PmK2MDV09-e1OMKTeyzvptOA6_O1xtHUE_w8ANai1NjrlJTVeQ04paoK8QiV0lGmDIubYucl7extnM1hozkNd5D0fqiapEoSeON7Y0I7LkalcY8-0EOQGa87hD8tSzd3xD2d7nPS4CJEDqOav7dIFZ8tZ4Pg13ZFaNPpMJ-owRECP8mmGcr-kPE6Q7y300BlSpFlsQYfiaw5xXa07o=')
 
 client = TelegramClient('user_bot', API_ID, API_HASH).start(session=SESSION_STRING)
 
