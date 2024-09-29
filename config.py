cf524ec7794c2e80682356dc37d2695e9e7927e6
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-29 16:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-import os
-
-API_ID = int(os.getenv("29264175"))
-API_HASH = os.getenv("0a3e78a4cd29529769faf5fe588f6de7")
-SESSION_STRING = os.getenv("1BVtsOHgBu8SqrLSmk5GJQKjoa9oVe174JD_3eKxpQhTjb6-yYYzTf0rq9rzwgVOdpK3QTyz2xMkn3BRyP65x_h-0DCaMBn8Q6b2wRkHEDl9Lbs3jHASnxICwQC2suCnKtmEGR2F-zWuc_K7MGj4NeNJipB1YHLjqbEfBDRbenCe2pbpfpDKXYltK8OCv028JtkAjUaSIBQmRnBusO--uodZvs4yiz1uwJPG7e3Gy4QuxRimS2m4E_HAq3_v6CY8TIczawkSvbowPXRdE3OKg2rHQSnyidK_F6hju_tbNtTkeffOrHAq2-U7WilzALwcdDTYWC05qFJrIo2n9vUr7D8MGueG0UW4=")
+# config.py
+API_ID = 29264175  # Your API ID as an integer
+API_HASH = '0a3e78a4cd29529769faf5fe588f6de7'  # Your API Hash as a string
+SESSION_STRING = '1BVtsOHgBu8SqrLSmk5GJQKjoa9oVe174JD_3eKxpQhTjb6-yYYzTf0rq9rzwgVOdpK3QTyz2xMkn3BRyP65x_h-0DCaMBn8Q6b2wRkHEDl9Lbs3jHASnxICwQC2suCnKtmEGR2F-zWuc_K7MGj4NeNJipB1YHLjqbEfBDRbenCe2pbpfpDKXYltK8OCv028JtkAjUaSIBQmRnBusO--uodZvs4yiz1uwJPG7e3Gy4QuxRimS2m4E_HAq3_v6CY8TIczawkSvbowPXRdE3OKg2rHQSnyidK_F6hju_tbNtTkeffOrHAq2-U7WilzALwcdDTYWC05qFJrIo2n9vUr7D8MGueG0UW4='  # Your string session
