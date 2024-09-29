24f121201f235ae7da546ce5fc7bafa9bbeed18f
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-29 14:29:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 import os
-import environs
 
-try:
-    env = environs.Env()
-    env.read_env("./.env")
-except FileNotFoundError:
-    print("No .env file found, using os.environ.")
-
-API_ID = int(os.getenv("API_ID", env.int("API_ID")))
-API_HASH = os.getenv("API_HASH", env.str("API_HASH"))
-SESSION_STRING = os.getenv("SESSION_STRING", env.str("SESSION_STRING"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_STRING = os.getenv("SESSION_STRING")
