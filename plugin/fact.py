05ead53d5735b9d7b133845e86ec2046a8efe301
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-29 08:55:22 +0530

    Delete plugin/fact.py

--- plugin/fact.py
+++
@@ -1,8 +0,0 @@
-from telethon import events
-import requests
-
-@events.register(events.NewMessage(pattern=r'\.fact'))
-async def fact(event):
-    response = requests.get("https://api.fungenerators.com/fact/random")
-    fact = response.json()["data"]["fact"]
-    await event.reply(f"Did you know? {fact}")
