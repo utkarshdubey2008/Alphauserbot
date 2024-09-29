46e9cd189c40cdffe58d42751a1fdb83158d9c09
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-29 14:59:20 +0530

    Update joke.py

--- plugin/joke.py
+++ plugin/joke.py
@@ -1,16 +1,15 @@
-# plugins/joke.py
-
-import requests
+import aiohttp
 from telethon import events
 
 @events.register(events.NewMessage(pattern=r'\.joke'))
 async def joke(event):
     """Fetches and sends a random joke from a joke API."""
     try:
-        response = requests.get("https://official-joke-api.appspot.com/random_joke")
-        joke_data = response.json()
-        setup = joke_data['setup']
-        punchline = joke_data['punchline']
-        await event.reply(f"😂 {setup} - {punchline}")
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://official-joke-api.appspot.com/random_joke") as response:
+                joke_data = await response.json()
+                setup = joke_data['setup']
+                punchline = joke_data['punchline']
+                await event.reply(f"😂 {setup} - {punchline}")
     except Exception as e:
         await event.reply(f"❌ Error fetching joke: {str(e)}")
