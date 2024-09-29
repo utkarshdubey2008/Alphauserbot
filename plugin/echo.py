65786ebbad54040bfc2143e1eb8bd3c41cf8e6c0
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-29 08:55:14 +0530

    Delete plugin/echo.py

--- plugin/echo.py
+++
@@ -1,6 +0,0 @@
-from telethon import events
-
-@events.register(events.NewMessage(pattern=r'\.echo (.+)'))
-async def echo(event):
-    text = event.pattern_match.group(1)
-    await event.reply(text)
