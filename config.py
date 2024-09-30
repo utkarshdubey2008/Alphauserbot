6efee93b8f17440ccb80e6c6617f090a8cd2c487
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-30 15:25:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
 import os
-from pyrogram import Client
 
 # Retrieve credentials from environment variables
-API_ID = int(os.getenv('API_ID'))  # Replace with your actual API ID
-API_HASH = os.getenv('API_HASH')    # Replace with your actual API Hash
+API_ID = int(os.getenv('API_ID'))  # Your API ID
+API_HASH = os.getenv('API_HASH')    # Your API Hash
 
-# Create the Pyrogram Client instance using in-memory session
-app = Client(
-    ":memory:",  # Use in-memory session
-    api_id=API_ID,
-    api_hash=API_HASH
-)
+# Your string session
+SESSION_STRING = "BQGCiQcAxQ6qgwYQHVA4Ysm9DYjmJrDCtcCWkNCITl0MoBOCa6KGTfggLs60K6Lj78vYuStou8nNekPlfWLa_HXHToMLASEaHSv3mz5S5gknLCRNjA5oA9evegCS56ZiqRqwY8i82OSkLkQJdHtH1DxRLavFk9dPUid9g3PyWZMX9Xc9xzhn6zZwveUqO6Jeo4OIgOcxEGoE6DvbmFPCTUc1-q9MG7OJcgPE5FLij5iblk8sqlXgQWgB37jQ5oI1hHxpmhvAVJIv7L20q8r57bGooA1kK5NkYalFscRjf7Tz-cGEMMFmv4PcKJadWRri7IGvY3YgrK6FiPMIomRoAfRmQPcnOgAAAAG9kgpPAA"
